746ab67a0d7606964bf245f9e2e3f01488bafc48
Elijah Green <elijah.khoury99@gmail.com>
2022-11-29 16:26:30 -0800

    commit tree 3479346b28f9a18aa7d3ce4e3130ec5e84821ad1

--- index.js
+++ index.js
@@ -1,1 +1,34 @@
-// Write your solution in this file!
+let employee = {name:'Elijah', streetAddress:'2020 Oak Road'}
+function updateEmployeeWithKeyAndValue (obj,key, value){
+    const newObj = {...obj}
+
+    newObj[key] = value;
+    
+    return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(obj, key, value){
+   
+    obj[key] = value; //
+   
+    return obj;
+}
+
+function deleteFromEmployeeByKey(employee, key){
+
+    const copyOfemployee = {...employee}
+
+    delete copyOfemployee[key]
+    
+    return copyOfemployee;
+    
+}
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+
+   delete employee[key]
+   return employee;
+   
+}
+
+
+// employee(...)
+// console.log(employees) 
